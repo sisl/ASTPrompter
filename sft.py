23f3e5d3d8d4280070bf7840bfc49ab56d87a6ba
ameliahardy <ahardy@stanford.edu>
2024-05-24 06:06:50 +0000

    fix saving

--- sft.py
+++ sft.py
@@ -118,7 +118,9 @@ if __name__ == "__main__":
         output_dir="sft_out/",
         learning_rate=5e-5,
         logging_first_step=True,
-        report_to="wandb"
+        report_to="wandb",
+        save_steps:2000,
+        load_best_model_at_end=True
     )
 
     trainer = Trainer(
