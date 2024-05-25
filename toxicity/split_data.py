010b04b874b0c1e49a216ad9460e15b995e4729d
ameliahardy <ahardy@stanford.edu>
2024-05-25 06:34:48 +0000

    split corpus into train/dev/test

---
+++ toxicity/split_data.py
@@ -0,0 +1,58 @@
+"""
+Split convokit data into train/dev/test (60/30/10)
+"""
+from convokit import Corpus, download, Conversation
+import json
+import jsonlines
+import random
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from reddit_data_helpers import filter_corpus_formatting, corpus_len
+from detoxify_reddit import jsonl_to_dict, filter_corpus_toxicity
+
+def filter_corpus_by_file(corpus:Corpus, fname:str):
+    with open(fname, "r") as f:
+        data = f.read()
+        ids = data.strip().split()
+        print(len(ids))
+        return corpus.filter_conversations_by(lambda x: x.id in ids)
+
+def split_corpus(corpus:Corpus, train_fname:str, dev_fname:str, test_fname:str):
+    # First filter corpus
+    id2results = jsonl_to_dict('detox_results.jsonl')
+    corpus = filter_corpus_toxicity(corpus, id2results, {"toxicity": 0.5})
+    corpus = filter_corpus_formatting(corpus)
+
+    # Now split corpus into train/dev/test
+    # 60/30/10
+    corpus_list = [conv for conv in corpus.iter_conversations()]
+    train, dev_test = train_test_split(corpus_list, train_size=0.6, random_state=24)
+    # 0.75 * 0.4 = 0.3
+    dev, test = train_test_split(corpus_list, train_size=0.75, random_state=24)
+    
+    # Write results to files for reproducibility
+    with open(train_fname, "w") as train_file:
+        for conv in train:
+            train_file.write(str(conv.id)+" ")
+
+    with open(dev_fname, "w") as dev_file:
+        for conv in dev:
+            dev_file.write(str(conv.id)+" ")
+    
+    with open(test_fname, "w") as test_file:
+        for conv in test:
+            test_file.write(str(conv.id)+" ")
+
+    # Print to sanity check
+    print("total examples post filtering",len(corpus_list))
+    print("# examples train",len(train))
+    print("# examples dev",len(dev))
+    print("# examples test",len(test))
+
+if __name__ == "__main__":
+    # download large conversation corpus
+    corpus = Corpus(filename=download("reddit-corpus-small"))
+
+    split_corpus(corpus, "data/train.txt", "data/dev.txt", "data/test.txt")
+    train_corpus = filter_corpus_by_file(corpus, "data/train.txt")
+    print(corpus_len(train_corpus))
