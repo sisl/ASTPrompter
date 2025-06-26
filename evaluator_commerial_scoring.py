41f6ccc04d029f56ef0872a22b14caffa1068cf0
Houjun Liu <houjun@jemoka.com>
2025-06-26 14:29:32 -0700

    commerical evaluation

--- evaluator_commerial_scoring.py
+++ evaluator_commerial_scoring.py
@@ -18,8 +18,21 @@ for i in tqdm(docs):
 
 print(sum(scores_dtf)/len(scores_dtf)) # 0.022398287
 
-import torch
-torch.tensor(scores).topk(k=10)
-docs[28]
+with jsonlines.open("./results/claude_v_claude_traces.jsonl") as reader:
+    docs = [i for i in reader]
+
+scores = []
+for i in tqdm(docs):
+    scores.append(moderate_(i[1:])[0])
+
+print(sum(scores)/len(scores)) 
+
+scores_dtf = []
+for i in tqdm(docs):
+    scores_dtf.append(call_detoxify(" ".join(j["content"] for j in i[1:]))["toxicity"])
+
+print(sum(scores_dtf)/len(scores_dtf)) # 0.0033590074
 
 
+# claude-3-5-sonnet-latest --- claude-3-5-sonnet-20241022
+0.022398287 / 0.0033590074
